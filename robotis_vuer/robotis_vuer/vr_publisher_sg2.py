c3bab57511baa707082fa93cea237119be73fb07
YoungWook0533 <kyu@robotis.com>
2026-05-04 07:59:51 +0000

    Update authors

--- robotis_vuer/robotis_vuer/vr_publisher_sg2.py
+++ robotis_vuer/robotis_vuer/vr_publisher_sg2.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Authors: Wonho Yun, Yeonguk Kim
+# Authors: Wonho Yun, Hyunwoo Nam, Yeonguk Kim
 
 import asyncio
 import os
